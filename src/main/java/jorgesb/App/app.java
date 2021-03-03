20b30b8d0fcfd944baeafe6b20a04bdfa76e7d5d
Adrián Manuel Reina Morales <bajodelresca@gmail.com>
2021-03-03 12:11:47 +0100

    Main

--- src/main/java/jorgesb/App/app.java
+++ src/main/java/jorgesb/App/app.java
@@ -5,12 +5,52 @@
  */
 package jorgesb.App;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import jorgesb.Utils.Utils;
+
 /**
  *
  * @author Alberto343
  */
 public class app {
+
     public static void main(String[] args) {
-        
+        //Host del servidor
+        final String HOST = "172.16.16.157";
+        //Puerto del servidor
+        final int PUERTO = 10578;
+        DataInputStream in;
+        DataOutputStream out;
+
+        try {
+            //Creo el socket para conectarme con el cliente
+            Socket sc = new Socket(HOST, PUERTO);
+
+            in = new DataInputStream(sc.getInputStream());
+            out = new DataOutputStream(sc.getOutputStream());
+
+            String nombre = Utils.getString("Introduce tu login: ");
+            String correo = Utils.getString("Introduce tu contraseña: ");
+
+            //Envio un mensaje al cliente
+            out.writeUTF(nombre);
+            out.writeUTF(correo);
+
+            //Recibo el mensaje del servidor
+            String mensaje = in.readUTF();
+
+            System.out.println(mensaje);
+
+            sc.close();
+
+        } catch (IOException ex) {
+            Logger.getLogger(app.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
     }
 }
